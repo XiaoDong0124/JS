0f8414d1661dd93da90d1b089e2c98f736eae37b
xiaodong <ilovemay1025@gmail.com>
2022-05-03 09:35:09 +0800

    third day1

--- 亂數選擇器/main.js
+++ 亂數選擇器/main.js
@@ -1,6 +1,11 @@
 $(function(){
     //console.log("Yo");
     $("input").on("click",function(){
-        alert("Hi");
+       // alert("Hi");
+       $("h1").text($("li").eq(Math.floor(Math.random()*$("li").length)).text())  //找到h1 設定文字變成Hello
     });
 });
+
+function xxx(){
+
+}
